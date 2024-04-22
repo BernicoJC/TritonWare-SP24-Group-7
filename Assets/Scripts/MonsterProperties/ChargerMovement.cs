4e5e860c62f5c3c93ed38a627c744f98c9c63f64
BernicoJC <108056736+BernicoJC@users.noreply.github.com>
2024-04-22 01:10:07 -0700

    Charger enemy

---
+++ Assets/Scripts/MonsterProperties/ChargerMovement.cs
@@ -0,0 +1,54 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChargerMovement : MonoBehaviour
+{
+    public GameObject player;
+    public float speed;
+    public float distanceToActivate;
+    public float chargeTime;
+    public float pauseTime;
+    public float extraRechargeTime;
+
+    private float distance;
+    private bool charging = false;
+
+    private float timer = 0;
+
+    // Update is called once per frame
+    void Update()
+    {
+        distance = Vector2.Distance(transform.position, player.transform.position);
+
+        if (charging == true) // for when it goes berserk and only wants blood
+        {
+            timer += Time.deltaTime;
+            if(timer > pauseTime + chargeTime + extraRechargeTime)
+            {
+                timer = 0;
+                charging = false;
+            }
+
+            else if (timer > pauseTime + chargeTime)
+            {
+
+            }
+
+            else if (timer > pauseTime)
+            {
+                transform.position = Vector2.MoveTowards(this.transform.position, player.transform.position, speed * Time.deltaTime);
+            }
+        }
+
+        if (charging == false && distance < distanceToActivate) // detect a player
+        {
+            charging = true;
+        }
+
+        if (charging == false && distance >= distanceToActivate) // detect a player
+        {
+            timer = 0;
+        }
+    }
+}
--- Assets/Scripts/MonsterProperties/MonsterDamage.cs
+++ Assets/Scripts/MonsterProperties/MonsterDamage.cs
@@ -7,12 +7,18 @@ public class MonsterDamage : MonoBehaviour
 {
     public int contactDamage;
     public Health playerHealth; // It seems using class in this way is importing from other script (just like Java)
+    public bool kamikaze;
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
         if(collision.gameObject.tag == "Player")
         {
             playerHealth.takeDamage(contactDamage);
+
+            if(kamikaze)
+            {
+                Destroy(gameObject);
+            }
         }
     }
 }

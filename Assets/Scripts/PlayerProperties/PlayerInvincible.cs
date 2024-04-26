a501df4fd8d65e5838750da1305c136644f5ef81
BernicoJC <108056736+BernicoJC@users.noreply.github.com>
2024-04-26 02:57:54 -0700

    monster flipping, health bar, invincibility frame

---
+++ Assets/Scripts/PlayerProperties/PlayerInvincible.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerInvincible : MonoBehaviour
+{
+    public float invincibleTime;
+    public float damagedTime;
+
+    private int LayerDefault;
+    private int LayerInvincible;
+
+    public void Start()
+    {
+        LayerDefault = LayerMask.NameToLayer("Default");
+        LayerInvincible = LayerMask.NameToLayer("Invincible");
+    }
+    public void applyInvincibility()
+    {
+        StartCoroutine(invincibilityFrame());
+    }
+
+    public IEnumerator invincibilityFrame()
+    {
+        // float old = gameObject.GetComponent<Rigidbody2D>().gravityScale;
+        gameObject.GetComponent<Rigidbody2D>().gravityScale = 0;
+        gameObject.GetComponent<Rigidbody2D>().velocity = new Vector2(0, 0);
+        gameObject.GetComponent<PlayerMovement>().cantMove = true;
+
+        gameObject.layer = LayerInvincible;
+
+        yield return new WaitForSeconds(damagedTime);
+
+        gameObject.GetComponent<Rigidbody2D>().gravityScale = gameObject.GetComponent<PlayerMovement>().gravity;
+        gameObject.GetComponent<PlayerMovement>().cantMove = false;
+
+        yield return new WaitForSeconds(invincibleTime - damagedTime);
+
+        gameObject.layer = LayerDefault;
+    }
+}
